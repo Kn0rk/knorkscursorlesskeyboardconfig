db553c42c13b9bfb1557826fdecc9f99702c62f6
Jan Scheffczyk <jan@scheffczyk.page>
2024-02-26 22:06:39 +0100

    moving kinda works

--- src/commands/bring.ts
+++ src/commands/bring.ts
@@ -1,22 +1,5 @@
 import { clearSelection, selectAll as selectAllTargets } from "../handler";
 import * as vscode from 'vscode';
-// export function clear(){
-//     let selection = selectAllTargets();
-//     if( selection.isSet){
-//         vscode.commands.executeCommand("editor.action.clipboardCopyAction");
-//     }
-//     selection.reset();
-//     vscode.commands.executeCommand("editor.action.clipboardPasteAction");
-//     clearSelection();
-// }
-
-// export async  function bring(){
-//     let selection = selectAllTargets();
-//     if( selection.isSet){
-//        await vscode.commands.executeCommand("editor.action.clipboardCutAction");
-//     }
-//     selection.reset();
-// }
 
 let tested_actions = [
     "editor.action.clipboardCutAction",
--- src/mods/basic.ts
+++ src/mods/basic.ts
@@ -20,20 +20,105 @@ export function modAll(modifier: string) {
         case "up":
             verticalMove("up");
             break;
+        case "nextToken":
+            byToken("next");
+            break;
+        case "prevToken":
+            byToken("prev");
+            break;
+        case "goToNextToken":
+            byToken("next","replace");
+            break;
+        case "goToPrevToken":
+            byToken("prev","replace");
+            break;
 
         default:
-            vscode.window.showErrorMessage(`$modifier in not supported`);
+            vscode.window.showErrorMessage(`Modifier ${modifier} not supported`);
             break;
     }
 }
 
-function home(){
+function byToken(
+    dir: "prev" | "next",
+    mode: "cursor" | "replace" = "cursor"
+) {
+
+
+    let targets = getTargets();
+    let newTargets = [];
+    for (let index = 0; index < targets.length; index++) {
+        const element = targets[index];
+        let cursorPos = element.range.end;
+        if (element.cursorPosition === "start") {
+            cursorPos = element.range.start;
+        }
+
+        const lineText = element.editor.document.lineAt(cursorPos.line).text;
+        let text = lineText.slice(cursorPos.character);
+        if (dir === "prev") {
+            text = lineText.slice(0, cursorPos.character);
+        }
+        const pattern = /([^a-zA-Z0-9\s]+[a-zA-Z0-9]*)|([a-zA-Z0-9]+)/g;
+        let match;
+        let matches = [];
+        while ((match = pattern.exec(text)) !== null) { matches.push(match); }
+
+        let newRange = null;
+        if (matches.length > 0 && dir === "next") {
+            let match = matches[0];
+            newRange = new vscode.Range(
+                new vscode.Position(cursorPos.line, cursorPos.character + match.index),
+                new vscode.Position(cursorPos.line, cursorPos.character + match.index + match[0].length)
+            );
+        }
+        else if (matches.length === 0 && dir === "next") {
+            newRange = new vscode.Range(
+                new vscode.Position(cursorPos.line + 1, 0),
+                new vscode.Position(cursorPos.line + 1, 0)
+            );
+        } else if (matches.length > 1 && dir === "prev") {
+            let lastMatch = matches[matches.length - 2];
+            newRange = new vscode.Range(
+                new vscode.Position(cursorPos.line, lastMatch.index),
+                new vscode.Position(cursorPos.line, lastMatch.index + lastMatch[0].length)
+            );
+        } else {
+            const lastCharInLine = element.editor.document.lineAt(cursorPos.line).range.end.character;
+            newRange = new vscode.Range(
+                new vscode.Position(cursorPos.line - 1, lastCharInLine),
+                new vscode.Position(cursorPos.line - 1, lastCharInLine)
+            );
+        }
+
+        if (mode === "cursor") {
+            if (element.cursorPosition === "end") {
+                newRange = new vscode.Range(
+                    element.range.start,
+                    newRange.end
+                );
+                newTargets.push(new UserTarget(element.editor, newRange, element.anchor, element.cursorPosition));
+            } else {
+                newRange = new vscode.Range(
+                    newRange.start,
+                    element.range.end,
+                );
+                newTargets.push(new UserTarget(element.editor, newRange, element.anchor, element.cursorPosition));
+            }
+        } else if (mode === "replace") {
+            newTargets.push(new UserTarget(element.editor, newRange, element.anchor, element.cursorPosition));
+        }
+    }
+    setRange(newTargets, "replace");
+}
+
+function home() {
     let targets = getTargets();
     let newTargets = [];
     for (let index = 0; index < targets.length; index++) {
         const element = targets[index];
         let line = element.range.end.line;
-        if (element.cursorPosition === "start"){
+        if (element.cursorPosition === "start") {
             line = element.range.start.line;
         }
 
@@ -41,24 +126,24 @@ function home(){
         let lineTxt = element.editor.document.lineAt(line).text;
         const match = lineTxt.match(/\S/);
 
-        if (match && match.index && match.index !==  element.range.end.character){
-            startOfLine = new vscode.Position(startOfLine.line,match.index);
+        if (match && match.index && match.index !== element.range.end.character) {
+            startOfLine = new vscode.Position(startOfLine.line, match.index);
         }
-        
+
         let range = new vscode.Range(
             element.range.start,
             startOfLine
         );
 
-        if (element.cursorPosition === "start"){
-            range= new vscode.Range(
+        if (element.cursorPosition === "start") {
+            range = new vscode.Range(
                 startOfLine,
                 element.range.end
             );
         }
-        newTargets.push(new UserTarget(element.editor, range,element.anchor,element.cursorPosition));
+        newTargets.push(new UserTarget(element.editor, range, element.anchor, element.cursorPosition));
     }
-    setRange(newTargets, "replace");   
+    setRange(newTargets, "replace");
 }
 
 function end() {
@@ -67,18 +152,18 @@ function end() {
     for (let index = 0; index < targets.length; index++) {
         const element = targets[index];
         let line = element.range.end.line;
-        if (element.cursorPosition === "start"){
+        if (element.cursorPosition === "start") {
             line = element.range.start.line;
         }
         let end = element.editor.document.lineAt(line).range.end;
         let range = new vscode.Range(element.range.start, end);
-        if (element.cursorPosition === "start"){
-            range= new vscode.Range(
+        if (element.cursorPosition === "start") {
+            range = new vscode.Range(
                 end,
                 element.range.end
             );
         }
-        newTargets.push(new UserTarget(element.editor, range,element.anchor,element.cursorPosition));
+        newTargets.push(new UserTarget(element.editor, range, element.anchor, element.cursorPosition));
     }
     setRange(newTargets, "replace");
 
@@ -90,28 +175,27 @@ function verticalMove(dir: "up" | "down") {
     for (let index = 0; index < targets.length; index++) {
         const element = targets[index];
         let cursorPos = element.range.end;
-        if (element.cursorPosition === "start")
-        {
-            cursorPos= element.range.start;
+        if (element.cursorPosition === "start") {
+            cursorPos = element.range.start;
         }
 
-        let nextPos = new vscode.Position(cursorPos.line+1,cursorPos.character);
+        let nextPos = new vscode.Position(cursorPos.line + 1, cursorPos.character);
         if (dir === "up") {
-            nextPos = new vscode.Position(cursorPos.line-1,cursorPos.character);
+            nextPos = new vscode.Position(cursorPos.line - 1, cursorPos.character);
         }
-        
-        if (new PositionMath(element.range.start).greaterThan( new PositionMath(nextPos))) {
+
+        if (new PositionMath(element.range.start).greaterThan(new PositionMath(nextPos))) {
             let range = new vscode.Range(
                 nextPos,
                 element.anchor,
             );
-            newTargets.push(new UserTarget(element.editor, range,element.anchor, "start"));
+            newTargets.push(new UserTarget(element.editor, range, element.anchor, "start"));
         } else {
             let range = new vscode.Range(
                 element.anchor,
                 nextPos
             );
-            newTargets.push(new UserTarget(element.editor, range, element.anchor,"end"));
+            newTargets.push(new UserTarget(element.editor, range, element.anchor, "end"));
         }
 
     }
