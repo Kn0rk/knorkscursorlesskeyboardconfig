80e9d7d97637de1470f3462c77199921e96a34f8
Jan Scheffczyk <jan@scheffczyk.page>
2024-02-26 22:14:57 +0100

    move hats to middle

--- src/hats/splitDocument.ts
+++ src/hats/splitDocument.ts
@@ -17,23 +17,29 @@ export function splitDocument(
 
     // split into lines
     const regEx = /[^\r\n]+/g;
-    let match;
-
-
-
     var results:HatCandidate[]=[];
-
+    let match;
     while (match = regEx.exec(text)){
+        if(!match){
+            continue;
+        }
         let sen = text.slice(match.index,match.index+match[0].length);
         const wordRegex = /\b\w+\b/g;
-        let firstWord = wordRegex.exec(sen);
-        if (!firstWord){
+        let inner_match;
+        let matches = [];
+        while ((inner_match = wordRegex.exec(sen)) !== null) { matches.push(inner_match); }
+
+        let target = matches[0];
+        if ( matches.length >1){
+            target = matches[1];
+        }
+        if (!target){
             continue;
         }
         const candidate:HatCandidate = {
-            startOffset:match.index+firstWord.index,
-            endOffset:match.index+firstWord.index+firstWord[0].length,
-            word:firstWord[0]
+            startOffset:match.index+target.index,
+            endOffset:match.index+target.index+target[0].length,
+            word:target[0]
         };
         results.push(candidate);
     }
